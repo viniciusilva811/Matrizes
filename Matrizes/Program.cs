e65ee9102b56778f040a233f4f3f1030267090f1
Vinícius Silva <vinisilva45@live.com>
2020-09-04 11:58:59 -0300

    Exercicio concluído

--- Matrizes/Program.cs
+++ Matrizes/Program.cs
@@ -15,11 +15,16 @@ namespace Matrizes
             int[,] matriz = new int[n, n];
 
             for (int i=0; i < n; i++) {
+                /*vai ler a linha e guardar o resultado no values e 
+                 usando split pra separar os numeros */
 
                 string[] values = Console.ReadLine().Split(' ');
+                /* o FOR vai percorrer o "values" e 
+                  guardando na matriz os elementos */
 
                 for (int j = 0; j < n; j++ ) {
-
+                    /*aqui percorre o vetor "values" já que é string 
+                     e convertendo em int e guardando nas linhas da matriz */
                     matriz[i, j] = int.Parse(values[j]);
                 }
             }
@@ -27,13 +32,16 @@ namespace Matrizes
             Console.WriteLine("Numeros na diagonal principal: ");
 
             for (int i = 0; i < n; i++) {
-
+             
                 Console.Write(matriz[i, i] + " ");
             }
             Console.WriteLine();
 
-            int count = 0;
 
+
+            /* abaixo é só uma comparação pra saber se é negativo ou não, 
+              caso seja, ele retorna a quantidade de negativos. */
+            int count = 0;
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
                     if (matriz[i, j] < 0) {
